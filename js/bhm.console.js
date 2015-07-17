8b0fde0f72b10c6b461d8c4ce470d49ea3b6b907
psalmody <psalmody@gmail.com>
2015-07-16 17:27:19 -0800

    finished functionality for console

--- js/bhm.console.js
+++ js/bhm.console.js
@@ -175,6 +175,7 @@
 
 //setup ckeditor styles
 (function(CKEDITOR, $) {
+    //css for CKEDITOR is every stylesheet on this page
     var cssfiles = $(document).find('link[rel="stylesheet"]');
     var arrcss = ['body{padding:5px;}'];
     cssfiles.each(function() {
@@ -188,43 +189,44 @@
 
 
 var BHM = (function(Vertebrate, $, my) {
-    //var my = {};
 
+    //clean the url for use as DOM id
     var clean = function(what) {
         if (typeof(what) != 'string') return what;
         return what.replace(/([.])|([/])|([ ])/g, '');
     }
 
+    //render a single page
     var renderPage = function( model ) {
         var $el = BHM.mc.$el;
         cleanfilename = clean(model.get('url'));
-        active = typeof(active) != 'undefined' ? active : false;
+        //add a tab
         $el.find('ul').append(tmpl($('#templateTabLI').html(),$.extend({},{clean:cleanfilename},model.attributes)));
-
+        //add a tab-content div
         $el.find('.tab-content').append(tmpl($('#templateTabDiv').html(),$.extend({},{clean:cleanfilename},model.attributes)));
 
-        if (active) {
-            $('#tab_'+cleanfilename).closest('li').addClass('active');
-            $('#_'+cleanfilename).addClass('active');
-        } else {
-            $el.find('ul li:first').addClass('active');
-            $el.find('.tab-content .tab-pane:first').addClass('active');
-        }
-
-        var tabContent = typeof(cleanfilename) == 'string' ? $('#_'+cleanfilename) : cleanfilename;
+        //first tab gets active
+        $el.find('ul li:first').addClass('active');
+        $el.find('.tab-content .tab-pane:first').addClass('active');
     }
 
+    //render helps by page
     var renderHelps = function( model ) {
         var $el = BHM.mc.$el;
+        //we'll be attaching to this tab content
         var tab = $('#_'+clean(model.get('url')));
+        //columns for JSONTable from mc settings
         var cols = BHM.mc.settings.columns.slice(0);
         cols.push(BHM.mc.settings.addButton);
+
+        //get all helps that are on this page
         var jsondata = [];
         var helps = BHM.ch.findAll(model.get('id'),'help_page_id');
         $.each(helps,function() {
             jsondata.push(this.attributes);
         });
 
+        //create JSONTable from group of helps
         $el.find('#_'+clean(model.get('url'))).JSONTable({
             data: jsondata,
             template: $('#templateHelperRow'),
@@ -236,8 +238,18 @@ var BHM = (function(Vertebrate, $, my) {
         });
     }
 
+    //render only one help
     var renderHelp = function( model ) {
         var $el = BHM.mc.$el;
+
+        //if there isn't a table yet, create one by running this as renderHelps
+        if ($('#_'+clean(model.get('filename'))).length) {
+            var page = BHM.cp.find(model.get('help_page_id'),'id');
+            renderHelps( page );
+            return true;
+        }
+
+        //get filename, find tbody and prepend a new row
         $('#_'+clean(model.get('filename')))
             .find('tbody')
             .prepend(
@@ -245,6 +257,7 @@ var BHM = (function(Vertebrate, $, my) {
             );
     }
 
+    //cp - pages collections - render
     my.cp = BHM.cp || {};
     my.cp.render = function() {
         var self = this;
@@ -252,24 +265,37 @@ var BHM = (function(Vertebrate, $, my) {
         var $el = BHM.mc.$el;
         $el.find('ul').empty();
         $.each(models,function() {
+            //render each page
             renderPage( this );
-        })
+        });
+        //add text and "Add New Page" button from template
+        $el.prepend($('#templateOpeningText').html());
     }
 
+    //ch - helps collection - render all
     my.ch = BHM.ch || {};
     my.ch.render = function() {
         var self = this;
         var pages = BHM.cp.models;
         var $el = BHM.mc.$el;
         $.each(pages,function() {
+            //render helps for each page
             renderHelps( this );
         })
     }
 
+    //publicize these functions
     my.renderHelp = function(model) {
         return renderHelp(model);
+    };
+    my.renderPage = function( model ) {
+        return renderPage(model);
+    };
+    my.clean = function( filename ) {
+        return clean(filename);
     }
 
+    //keep track of console and settings in BHM.mc
     my.mc = {
         settings: {
             addButton: '<button class="btn btn-sm btn-block btn-default addHelper">Add</button>',
@@ -277,9 +303,9 @@ var BHM = (function(Vertebrate, $, my) {
             ajaxFail: false,
             templates: 'templates/bhm.console.html'
         },
-        $el: '',
-        callbacks: $.Callbacks(),
+        $el: '', // jQuery object which the console isn't put in
         render: function() {
+            //render this object - setup a few important things
             var self = this;
             //bootstrap tab click functions
             $('#helpsManager').on('click', '.nav.nav-tabs a', function(e) {
@@ -287,24 +313,14 @@ var BHM = (function(Vertebrate, $, my) {
                 $(this).tab('show')
             });
 
-            //get templates
-            var dfd = $.get(self.settings.templates, function(data) {
-                $(data).appendTo('body');
+            //get templates and setup CKEDITOR in modal
+            var dfd = $.get(self.settings.templates);
 
-                //setup modal dialog
-                CKEDITOR.replace('bhmTextareaEditor');
-                $('#bhmModal').find('.btn-save-html').click(function() {
-                    var modal = $('#bhmModal'),
-                        model = BHM.ch.find(modal.data('helpId'),'id');
-                    model.set('html',CKEDITOR.instances['bhmTextareaEditor'].getData());
-                    CKEDITOR.instances['bhmTextareaEditor'].setData('');
-                    $('#bhmModal').modal('hide');
-                })
-            });
-
-            //setup tabpanel, then fetch collections
+            //setup tabpanel, CKEDITOR in modal when templates are loaded, then fetch collections
             $.when(dfd)
-                .then(function() {
+                .then(function( data ) {
+                    $(data).appendTo('body');
+                    CKEDITOR.replace('bhmTextareaEditor');
                     self.$el.append(tmpl($('#templateTabPanel').html(), {}));
                 })
                 .then(function() {
@@ -329,10 +345,7 @@ var BHM = (function(Vertebrate, $, my) {
         mc.$el = this;
         mc.render();
 
-
         var self = this;
-
-
 
         var getPageFor = function( $obj ) {
             return BHM.cp.find($obj.closest('.tab-pane').data('page-id'),'id');
@@ -347,10 +360,6 @@ var BHM = (function(Vertebrate, $, my) {
 
         $(document).on('vertebrate:fetched', function(e, c, ms) {
             c.render();
-            if (!$('#tab_NewPage').length) {
-                var template = tmpl($('#templateTabLI').html(),{clean:'NewPage',url:'New Page'});
-                self.find('ul').append(template);
-            }
         }).on('vertebrate:changeattr',function(e,m,mattr,mchanged) {
             getElForHelp(m).find('.saveHelp').addClass('btn-warning');
         });
@@ -376,11 +385,26 @@ var BHM = (function(Vertebrate, $, my) {
             var model = getHelpFor($(this));
             BHM.ch.remove(model);
             $.when(model.delete()).done(function() {
+                if (!BHM.ch.findAll(model.get('help_page_id'),'help_page_id')) {
+                    var page = BHM.cp.find(model.get('help_page_id'),'id');
+                    $.when(page.delete()).done(function() {
+                        var cleanurl = BHM.clean(page.get('url'));
+                        $.when($('#_'+cleanurl).fadeOut(300)).then(function() {
+                            return $('#tab_'+cleanurl).closest('li').fadeOut(300);
+                        }).then(function() {
+                            $('#tab_'+cleanurl).closest('li').remove();
+                            $('#_'+cleanurl).remove();
+                            self.find('.nav-tabs li:first a').click();
+                        })
+
+                    });
+                }
                 getElForHelp(model)
                 .fadeOut(300,function() {
                     $(this).remove();
                 })
             });
+
         }).on('click','.addHelper',function() {
             var page = getPageFor($(this));
             var model = new BHM.helper({
@@ -399,263 +423,39 @@ var BHM = (function(Vertebrate, $, my) {
             CKEDITOR.instances['bhmTextareaEditor'].setData(model.get('html'));
             modal.data('helpId',model.get('id'));
             modal.modal();
-        });
-
-    }
-}(jQuery));
-
-
-
-/*
-
-(function($) {
-    $('#helpsManager').on('click','.nav.nav-tabs a',function(e) {
-        e.preventDefault()
-        $(this).tab('show')
-    })
-
-    $.fn.BHMConsole = function( opts ) {
-        var helpconsole = this;
-        var settings = helpconsole.settings;
-        var self = this;
-
-        helpconsole.settings = $.extend({},{
-            addButton: '<button class="btn btn-sm btn-block btn-default addHelper">Add</button>',
-            columns: ['Field Selecter','Modal Title','Size','Content','Save'],
-            ajaxFail: false,
-            templates: 'templates/bhm.console.html'
-        }, helpconsole.settings, opts);
-
-        this._init = function( opts ) {
-
-            //setup tab layout
-            setupTabs();
-
-            //setup vertebrate view
-            setupVertebrate();
-
-
-        }
-
-
-
-
-
-        var addNewTab = function() {
-            var template = tmpl($('#templateTabLI').html(),{clean:'NewPage',url:'New Page'});
-            self.find('ul').append(template);
-        }
-
-        var makeTab = function( page, active ) {
-            cleanfilename = clean(page.get('url'));
-            active = typeof(active) != 'undefined' ? active : false;
-            self.find('ul').append(tmpl($('#templateTabLI').html(),$.extend({},{clean:cleanfilename},page.attributes)));
-
-            self.find('.tab-content').append(tmpl($('#templateTabDiv').html(),$.extend({},{clean:cleanfilename},page.attributes)));
-
-            if (active) {
-                $('#tab_'+cleanfilename).closest('li').addClass('active');
-                $('#_'+cleanfilename).addClass('active');
-            } else {
-                self.find('ul li:first').addClass('active');
-                self.find('.tab-content .tab-pane:first').addClass('active');
-            }
-
-            var tabContent = typeof(cleanfilename) == 'string' ? $('#_'+cleanfilename) : cleanfilename;
-        }
-
-        var makeTable = function( obj, page ) {
-            var cols = self.settings.columns.slice(0);
-            cols.push(self.settings.addButton);
-            var jsondata = [];
-            var helps = BHM.ch.findAll(page.get('id'),'help_page_id');
-            $.each(helps,function() {
-                jsondata.push(this.attributes);
+        }).on('click','#BHMaddPage',function() {
+            var url = prompt('Enter the relative url of the page to add to:');
+            if (url.length < 1) return false;
+            var id = BHM.cp.next('id').toString();;
+            var page = new BHM.page({
+                "id": id,
+                "url": url
             });
-
-            obj.JSONTable({
-                data: jsondata,
-                template: $('#templateHelperRow'),
-                templateParams: {'filename':page.get('url')},
-                columns: cols,
-                success: function() {
-                    $('.helpHTML').hide();
-                }
-            });
-            return true;
-        }
-
-
-        //initatiate once templates are loaded
-        $.when(dfd).done(function() {
-            self._init();
-        })
-
-        self.on('click','.addHelper',function() {
-            var page = getPageFor($(this));
-            addHelp( page );
-        }).on('click','.editHelp',function() {
-            editHelp(getHelpFor($(this)));
-        }).on('click','.saveHelp',function() {
-            var help = getHelpFor($(this));
+            var help = new BHM.helper({
+                "id": BHM.ch.next('id'),
+                "filename": url,
+                "help_page_id": id
+            })
+            BHM.cp.add(page);
+            BHM.ch.add(help);
+            BHM.renderPage(page);
+            BHM.renderHelp(help);
+            page.save();
             help.save();
-            markSaved( help );
-        }).on('click','.deleteHelp',function() {
-            deleteHelp( getHelpFor($(this)) );
-        }).on('change','input[type="text"]',function() {
-            var help = getHelpFor($(this));
-            help.set($(this).data('attr'),$(this).val());
-            if ($(this).val() != $(this).data('value')) markEdited(help);
-        }).on('change','input[type="checkbox"]',function() {
-            var help = getHelpFor($(this));
-            var value = $(this).is(':checked') ? 1 : 0;
-            help.set('large',value);
-            if (value != $(this).data('value')) markEdited(help);
-        }).on('click','#tab_NewPage',function() {
-            addNewPage();
-        }).on('click','ul.nav-tabs a',function(e) {
-            e.preventDefault();
-            if ($(this).prop('id') != 'tab_NewPage') {
-                $(this).tab('show');
-            }
+            $('#tab_'+BHM.clean(url)).click();
         });
 
-        var ckeditorcount=0;
-
-        function addHelp( page ) {
-            var newhelp = new BHM.helper({});
-            newhelp.set('id',(Number(BHM.ch.max('id'))+1).toString());
-            BHM.ch.add(newhelp);
-            console.log(newhelp.get());
-            var tbody = $('#_'+clean(page.get('url'))+' tbody');
-            var tr = $(tmpl($('#templateHelperRow').html(),$.extend(newhelp.get(),{filename:page.get('url')})));
-            tbody.prepend(tr);
-            markEdited(newhelp);
-        }
-
-        function editHelp( help ) {
-            modal = tmpl($('#templateModal').html(),{
-                'filename': help.get('filename'),
-                'field_selecter': help.get('field_selecter'),
-                'html': help.get('html'),
-                'id': help.get('id'),
-                'random': ckeditorcount
-            });
-            var modal = $(modal).modal().on('shown.bs.modal',function(e) {
-                CKEDITOR.replace('ckeditor'+ckeditorcount);
-                //CKEDITOR.add
-
-            }).on('hide.bs.modal',function() {
-                CKEDITOR.instances['ckeditor'+ckeditorcount].destroy();
-                ckeditorcount++;
-            }).on('click','.btn-save-html',function() {
-                var ckid = 'ckeditor'+ckeditorcount;
-                var ckhtml = CKEDITOR.instances[ckid].getData();
-                if (help.get('html').replace(/\n/g,"") !== ckhtml.replace(/\n/g,"")) {
-                    help.set('html',ckhtml);
-                    markEdited( help );
-                }
-                modal.modal('hide');
-            });
-        };
-
-        function markEdited( help ) {
-            $('#help'+help.get('id')+' .saveHelp').addClass('btn-warning');
-        }
-
-        function markSaved( help ) {
-            $('#help'+help.get('id')+' .saveHelp').removeClass('btn-warning').addClass('btn-success');
-            setTimeout(function() {
-                $('#help'+help.get('id')+' .saveHelp').removeClass('btn-success');
-            },5000)
-        }
-
-        function deleteHelp( help ) {
-            var tr = $('#help'+help.get('id'));
-            tr.addClass('danger');
-            var sure = confirm('Are you sure you want to delete this helper?');
-            if (!sure) {
-                tr.removeClass('danger');
-                return false;
-            };
-
-            help.delete();
-            BHM.ch.remove(help);
-            tr.fadeOut(500,function() {
-                $(this).remove();
-                //better check if this is the last help on that page and delete page
-                var allhelps = BHM.ch.find(help.get('help_page_id'),'help_page_id');
-                if (!allhelps) {
-                    var page = BHM.cp.find(help.get('help_page_id'),'id');
-                    page = page[0];
-                    removePage(page);
-                    page.delete();
-                    BHM.cp.remove(page);
-                }
-            });
-        }
-
-        function addNewPage() {
-            var url = prompt('Copy full url after domain name:');
-            if(!url) {
-                self.find('ul.nav-tabs li:first').click();
-                return false;
-            }
-            var newpage = new BHM.page();
-            newpage.set('url',url);
-            newpage.set('id',Number(BHM.cp.max('id'))+1);
-            BHM.cp.add(newpage);
-            newpage.save();
-            newpage.fetch();
-            BHM.cp.add(newpage);
-            var newhelp = new BHM.helper();
-            newhelp.set('help_page_id',newpage.get('id'));
-            newhelp.set('id',Number(BHM.ch.max('id'))+1);
-            newhelp.set('filename',url);
-            BHM.ch.add(newhelp);
-            newhelp.save();
-            makeTab(newpage);
-            $('#tab_'+clean(url)).click();
-            $('#tab_NewPage').closest('li').detach().appendTo(self.find('ul.nav-tabs'));
-        }
-
-        function removePage( page ) {
-            var cleanurl = clean(page.get('url'));
-            $('#_'+cleanurl).fadeOut(500,function() {
-                $(this).remove();
-                $('#tab_'+cleanurl).fadeOut(500,function() {
-                    $(this).remove();
-                    self.find('ul.nav-tabs li a:first').click();
-                })
-            });
-        }
-
-        function getHelpFor( obj ) {
-            var tr = obj.closest('tr'),
-                help = tr.data('id');
-            return BHM.ch.find( help.toString(), 'id' );
-        }
-
-        function getPageFor( obj ) {
-            return BHM.cp.find(obj.closest('.tab-pane').data('id').toString(),'id');;
-        }
-
-        function setupVertebrate() {
-
-
-
-
-        }
-
+        //setup modal dialog
+        $('body').on('click','#bhmModal .btn-save-html', function() {
+            var modal = $('#bhmModal'),
+                model = BHM.ch.find(modal.data('helpId'),'id');
+            model.set('html',CKEDITOR.instances['bhmTextareaEditor'].getData());
+            CKEDITOR.instances['bhmTextareaEditor'].setData('');
+            $('#bhmModal').modal('hide');
+        });
 
     }
-
-
-
 }(jQuery));
-
-
-*/
 
 // Prevent bootstrap dialog from blocking focusin - necessary for CKEDITOR
 $(document).on('focusin', function(e) {
